0231d6f0c8d9451a12cfec7668acf460e93b6422
Luana Domeneghetti <158210063+domeneghettii@users.noreply.github.com>
2025-02-25 10:54:44 -0300

    update post

--- src/controller/postsController.js
+++ src/controller/postsController.js
@@ -3,10 +3,10 @@ const PostList = require("../models/PostList");
 
 const lista = new PostList();
 
-const post1 = new Post(1136, "Outer Banks", "A série se concentra na trajetória de um grupo de adolescentes residentes na Carolina do Norte, que se autodenominam Pogues.", "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.techtudo.com.br%2Flistas%2F2023%2F02%2Fouter-banks-conheca-personagens-e-elenco-da-serie-da-netflix-streaming.ghtml&psig=AOvVaw2_YsluQrSJY0yvqAMJV3C7&ust=1740573419579000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCIDRxLXr3osDFQAAAAAdAAAAABAE", 5000, "Muito bom, amo essa série!!");
+const post1 = new Post("Outer Banks", "A série se concentra na trajetória de um grupo de adolescentes residentes na Carolina do Norte, que se autodenominam Pogues.", "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.techtudo.com.br%2Flistas%2F2023%2F02%2Fouter-banks-conheca-personagens-e-elenco-da-serie-da-netflix-streaming.ghtml&psig=AOvVaw2_YsluQrSJY0yvqAMJV3C7&ust=1740573419579000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCIDRxLXr3osDFQAAAAAdAAAAABAE", 5000, "Muito bom, amo essa série!!");
 lista.addPost(post1);
 
-lista.addPost( new Post(2810, "The Vampire Diaries", "A série segue a vida de Elena Gilbert (Nina Dobrev), uma jovem estudante de 17 anos, que após perder seus pais em um acidente de carro, começou a lidar com o luto e a tristeza. Elena se apaixona por um vampiro chamado Stefan Salvatore (Paul Wesley) e os dois começam um relacionamento.", "https://www.google.com/url?sa=i&url=https%3A%2F%2Freportersombra.com%2Fthe-vampire-diaries-diarios-de-um-vampiro%2F&psig=AOvVaw2BctFjPvK_ZZoA0cfXmBzI&ust=1740573674780000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCMCwxq7s3osDFQAAAAAdAAAAABAE", 7000, "Maravilhoso, mas delena perdeu a química no final..." ));
+lista.addPost( new Post("The Vampire Diaries", "A série segue a vida de Elena Gilbert (Nina Dobrev), uma jovem estudante de 17 anos, que após perder seus pais em um acidente de carro, começou a lidar com o luto e a tristeza. Elena se apaixona por um vampiro chamado Stefan Salvatore (Paul Wesley) e os dois começam um relacionamento.", "https://www.google.com/url?sa=i&url=https%3A%2F%2Freportersombra.com%2Fthe-vampire-diaries-diarios-de-um-vampiro%2F&psig=AOvVaw2BctFjPvK_ZZoA0cfXmBzI&ust=1740573674780000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCMCwxq7s3osDFQAAAAAdAAAAABAE", 7000, "Maravilhoso, mas delena perdeu a química no final..." ));
 
 const router = {
     getAllPosts: (req, res) => {
--- src/models/Post.js
+++ src/models/Post.js
@@ -1,7 +1,7 @@
 const { v4: uuid4 } = require('uuid');
 class Post{
-    constructor(id, title, description, url, like, coments) {
-        this.id = id;
+    constructor(title, description, url, like, coments) {
+        this.id = uuid4();
         this.title = title;
         this.description = description;
         this.url = url;
