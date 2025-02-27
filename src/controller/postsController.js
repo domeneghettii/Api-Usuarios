b097659f82b4e3b4c8b42e7e718ac74fc2d83b65
Luana Domeneghetti <158210063+domeneghettii@users.noreply.github.com>
2025-02-27 13:30:11 -0300

    feat: add img POST - ATUALIZAR

--- src/controller/postsController.js
+++ src/controller/postsController.js
@@ -44,8 +44,7 @@ const router = {
 
     updatePost: (req, res) => {
         try {
-            const updatedPost = lista.updatePost(req.params.id, req.body);
-            res.status(200).json(updatedPost);
+            res.status(200).json(lista.updatePost(req.params.id, req.body));
         } catch (error) {
             res.status(404).json({message: "erro ao atualizar post :(", error});
         }
