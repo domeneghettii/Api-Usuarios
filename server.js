71490cfae05a03b164f7ebefe2c07ce92917deda
Luana <luanaa.domeneghettii@gmail.com>
2025-04-17 18:17:20 -0300

    update server.js

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use('/api', usersRoutes);
 app.use('/api', postRoutes);
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Servidor rodando na porta http://localhost:${PORT}`);
 });
